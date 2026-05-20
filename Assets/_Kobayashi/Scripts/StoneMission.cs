b27b6ccf3491d5cbc6f125dddfd0c737eaaf8937
KobayashiTetsuya <qtetsu.8201.tetsuq@gmail.com>
2026-05-21 00:44:06 +0900

    [add]ミッション実装

---
+++ Assets/_Kobayashi/Scripts/StoneMission.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public  class StoneMission : MissionObjectBase
+{
+    public override void CrearAnimation()
+    {
+        base.CrearAnimation();
+
+        //Šâ‚ª‚Á”ò‚Ô‰‰o
+    }
+}
