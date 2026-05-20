89d40b2510d3841f635e45b17380e612127d0a4f
hiaki0617 <hiaki0617@gmail.com>
2026-05-20 23:40:03 +0900

    [add]ゲームマネージャーでプレイヤーの位置をスコアとして表示するように変更 スコアマネージャーを表示専用にするたScoreViewManegerに変更

--- Assets/_Kobayashi/Scripts/GameManager.cs
+++ Assets/_Kobayashi/Scripts/GameManager.cs
@@ -26,7 +26,9 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }
     public StageType CurrentStageType { get; private set; }
     public bool IsDead { get; private set; } = false;
+    public int Score { get; private set; } = 0;
 
+    [SerializeField] private GameObject _player;
     [SerializeField] public FuelObjectPool FuelObjectPool;
     [SerializeField] public Canvas Canvas;
     [SerializeField] private FireBox _fireBox;
@@ -35,8 +37,9 @@ public class GameManager : MonoBehaviour
     [SerializeField] private float _decreaseFire = -0.2f;
 
     private bool _isDisplay = false;
-    private ScoreManager _scoreManager;
+    private ScoreViewManager _scoreManager;
     private DeathType _deathType;
+
 
     private void Awake()
     {
@@ -48,13 +51,14 @@ public class GameManager : MonoBehaviour
     {
         CurrentStageType = StageType.Normal;
         IsDead = false;
-        _scoreManager = FindAnyObjectByType<ScoreManager>();
+        _scoreManager = FindAnyObjectByType<ScoreViewManager>();
     }
     // Update is called once per frame
     void Update()
     {
         if (!IsDead)
         {
+            ScoreUpdate();
             _fireBox.DecreaseFire(_decreaseFire);
         }
         else
@@ -66,16 +70,20 @@ public class GameManager : MonoBehaviour
             _isDisplay = true;
         }
 
-        if(_fireBox.CurrntFire <= 0)
+        if (_fireBox.CurrntFire <= 0)
         {
             _deathType = DeathType.Stop;
             IsDead = true;
         }
-        else if(_fireBox.CurrntFire >= 100)
+        else if (_fireBox.CurrntFire >= 100)
         {
             _deathType = DeathType.OverHeat;
             IsDead = true;
         }
+    }
+    private void ScoreUpdate()
+    {
+        Score = (int)_player.transform.position.z;
     }
 
     public void StageChange(StageType nextStage)
--- Assets/_Kobayashi/Scripts/ScoreViewManager.cs
+++ Assets/_Kobayashi/Scripts/ScoreViewManager.cs
@@ -2,19 +2,30 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScoreManager : MonoBehaviour
+public class ScoreViewManager : MonoBehaviour
 {
     public ResultDataBase ResultDatas => _resultDatas;
 
     [Header("-----éQè∆-----")]
     [SerializeField] private GameObject _scorePanel;
-    [SerializeField] private TextMeshProUGUI _score;
+    [SerializeField] private TextMeshProUGUI _scoreResultText;
     [SerializeField] private Image _backGround;
     [SerializeField] private ResultDataBase _resultDatas;
+    [Header("-----ÉCÉìÉQÅ[ÉÄ-----")]
+    [SerializeField] private TextMeshProUGUI _scoreIngameText;
 
     public void DisplayResult(DeathType deathType)
     {
         _scorePanel.SetActive(true);
         _backGround.sprite = _resultDatas.GetCardData(deathType).Sprite;
+        _scoreResultText.text = $"{GameManager.Instance.Score}m";
+    }
+    private void IngameView()
+    {
+        _scoreIngameText.text = $"{GameManager.Instance.Score}m";
+    }
+    private void Update()
+    {
+        IngameView();
     }
 }
