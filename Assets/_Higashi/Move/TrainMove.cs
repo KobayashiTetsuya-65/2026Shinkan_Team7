9860baaf5d1d645848e9543db88efc314f8da66a
hiaki0617 <hiaki0617@gmail.com>
2026-05-20 21:06:17 +0900

    [fix]列車の移動をベロシティに変更

--- Assets/_Higashi/Move/TrainMove.cs
+++ Assets/_Higashi/Move/TrainMove.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class TrainMove : MonoBehaviour
 {
-   // [SerializeField] private float
+    [SerializeField] private FireBox _fireBox;
+    [SerializeField] private float _moveSpeed = 1f;
+
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        _rb.linearVelocity = Vector3.down * _moveSpeed * _fireBox.CurrntFire * Time.deltaTime;
+    }
 }
