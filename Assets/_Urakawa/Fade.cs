b3d14e849cfc66aa8ae2f619f5bd59417b487ca7
puchuanyuan9 <puchuanyuan9@gmail.com>
2026-05-20 16:20:46 +0900

    add フェードアウトの後にシーンが変わる仕組みを作りました。

---
+++ Assets/_Urakawa/Fade.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEditor.Rendering;
+
+/// <summary>
+/// フェードインフェードアウトする
+/// </summary>
+public class Fade : MonoBehaviour
+{
+    public Image _canvasGroup;
+
+    public void FadeOut()
+    {
+        _canvasGroup.DOFade(1, 2f).OnComplete(Change_button);
+
+    }
+    private void Start()
+    {
+        FadeOut();
+    }
+    [SerializeField] private string _sceneName;   
+    public void Change_button()
+    {
+         ChangeScene.Instance.SceneLoad(_sceneName);
+    }
+    
+}
